0a053356db05f9863afa408410ea682e8857a4fd
byynaryy <endeki@gmail.com>
2020-12-17 19:32:01 +0100

    Create a textarea element in a vue component

---
+++ src/components/OpenEnded.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="kaka">
+      <h1>Open-Ended</h1>
+      <span>Multiline message is:</span>
+      <p style="white-space: pre-line;">{{ message }}</p>
+      <br>
+      <textarea v-model="message" placeholder="add multiple lines"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OpenEnded',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Questionnaire.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="open">
+    <OpenEnded />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import OpenEnded from '@/components/OpenEnded.vue';
+
+export default {
+  name: 'Questionnaire',
+  components: {
+    OpenEnded,
+  },
+};
+</script>
